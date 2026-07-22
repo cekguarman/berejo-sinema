221003cfe3f1f0012e12f55bdb21a7fd760c476c
Armansyah, S.Kom, M.Pd, Gr. <40592177+cekguarman@users.noreply.github.com>
2026-07-22 13:42:25 +0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './berejo-sinema' // Ubah dari './src/berejo_gambar' menjadi './berejo_gambar'
-import './index.css'             // Ubah dari './src/index.css' menjadi './index.css'
+import App from './berejo_sinema' // Sesuaikan dengan nama file .tsx baru Anda di folder src/
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
