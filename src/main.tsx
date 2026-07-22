f8342c07671315b1d1ae42755f172688188d3dae
Armansyah, S.Kom, M.Pd, Gr. <40592177+cekguarman@users.noreply.github.com>
2026-07-22 13:11:35 +0700

    Create main.tsx

---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './berejo-sinema' // Ubah dari './src/berejo_gambar' menjadi './berejo_gambar'
+import './index.css'             // Ubah dari './src/index.css' menjadi './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
